fa7263a0bbb44b2ce87831a7fea3652f01a2457c
tono77 <edgardo.ochoa@gmail.com>
2017-12-20 10:42:17 -0300

    Cambio sistema de grillas

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'pages/home'
 
   get 'pages/about'
-
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
